c309568e60956d030d8a2a77f5ac38d17627e544
Talha Rai <talhakamalia@gmail.com>
2019-02-21 18:23:39 +0500

    add Ellipsis and existInThisRadius'

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # behnaNPM
 This package is about some cool funtions like
 ## Funtions
+-  [Ellipsis](#About)
+-  [existInThisRadius](#About)
 -  [getSize](#About)
 
 ## Installation 
@@ -9,11 +11,27 @@ npm i behna
 ```
 ## Example
 ```sh
-import {getSize} from 'behna';
-console.log("result",getSize(obj));
+import {getSize, existInThisRadius, Ellipsis} from 'behna';
+console.log("getSize =",getSize(obj));
+console.log("existInThisRadius =",existInThisRadius({ lat: 31.449865899999995, lng: 74.2968439 }, { lat: 31.5204, lng: 74.3587 }, 10));
+console.log("Ellipsis =",<Ellipsis title="Ms Water Internship" limit="15" />);
 ```
 
 ## About
-getSize tells size(Memory) of oj that <br>
-[Npm Package](https://www.npmjs.com/package/behna)
+**getSize** tells size(Memory) of oj that <br>
+**existInThisRadius** tells You location Exist in Radius or not 
+```
+Params(myLocation,AreaLocation,km)
+mylocation is my lat, lng 
+AreaLocation is Region or City Like Lahore lat lng
+km is Radius Size in Kilimeters 
+```
+**Ellipsis** will show `...` in the end of line after given limit
 
+```
+<Ellipsis title="Ms Water Internship" limit="15" />
+```
+
+### License
+
+Behna is [MIT licensed](./LICENSE).
--- index.js
+++ index.js
@@ -1,41 +1,48 @@
-const behna =(string) => {
-    if (typeof string !== "string") throw new TypeError("Behna wants a string!");
-    return string.replace(/\s/g, "");
-  };
- 
-const getSize = (object)=> {
+const existInThisRadius = (checkPoint, centerPoint, km) => {
+    var ky = 40000 / 360;
+    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+    return Math.sqrt(dx * dx + dy * dy) <= km;
+}
+
+const Ellipsis = (props) => {
+    return ((props.title).length > props.limit) ? (((props.title).substring(0, props.limit)) + '..') : props.title
+}
+
+const getSize = (object) => {
     var objectList = [];
-    var stack = [ object ];
+    var stack = [object];
     var bytes = 0;
 
-    while ( stack.length ) {
+    while (stack.length) {
         var value = stack.pop();
 
-        if ( typeof value === 'boolean' ) {
+        if (typeof value === 'boolean') {
             bytes += 4;
         }
-        else if ( typeof value === 'string' ) {
+        else if (typeof value === 'string') {
             bytes += value.length * 2;
         }
-        else if ( typeof value === 'number' ) {
+        else if (typeof value === 'number') {
             bytes += 8;
         }
         else if
-        (
+            (
             typeof value === 'object'
-            && objectList.indexOf( value ) === -1
-        )
-        {
-            objectList.push( value );
+            && objectList.indexOf(value) === -1
+        ) {
+            objectList.push(value);
 
-            for( var i in value ) {
-                stack.push( value[ i ] );
+            for (var i in value) {
+                stack.push(value[i]);
             }
         }
     }
     return bytes;
-  };
-  export {
-      behna,
-      getSize
-  }
+};
+export {
+    getSize,
+    existInThisRadius,
+    Ellipsis
+}
